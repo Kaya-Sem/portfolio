ce5b4cce1b47f0d3469cd3eb5a91cda322ec914f
Kaya-Sem <kayasemvc@gmail.com>
2025-05-13 22:29:38 +0200

    updated edge link

--- src/config/graphConfig.js
+++ src/config/graphConfig.js
@@ -56,7 +56,7 @@ export const graphConfig = {
     },
     link: {
       iterations: 10,
-      distance: 200,
+      distance: 130,
       strength: 0.3,
     },
   },
@@ -71,13 +71,14 @@ export const graphConfig = {
   },
 
   pdfPaths: {
-    "2024-0356d86e0f434447a5aa19089ae2dcd": "/spec/commandtrein/commandtrein.pdf",
+    "2024-0356d86e0f434447a5aa19089ae2dcd":
+      "/spec/commandtrein/commandtrein.pdf",
     "2025-a9566dacc1e6455ebb7fff144d6e7e50": "/spec/ouroboros.pdf",
     "2025-0d20a3c04bad4366bf78ad9ff2e607d2": "/spec/semlink.pdf",
     "2025-a9deb489bda540d09765aa66054da98b": "/spec/gent_nachtleven.pdf",
     "2024-7a49c319ffbd411999e12bf7f248aeed": "/spec/oopsie.pdf",
     "2025-662550edefdb423ea480deead8279d1b": "/spec/friendsync.pdf",
-    "2025-b0c866ac55e6441caf619a98d9a8c6f8": "/spec/kayasemdotcom/kayasemdotcom.pdf",
+    "2025-b0c866ac55e6441caf619a98d9a8c6f8":
+      "/spec/kayasemdotcom/kayasemdotcom.pdf",
   },
 };
-
